97787f16516b2c363975916baaf7a8f3bbdfde69
Janis Basis Basovs <janis_basis@hotmail.com>
2020-08-24 00:06:29 +0300

    goodnight

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,22 +1,35 @@
-import React from "react";
-import Card from "./Card";
-import NewspaperList from "../lib/NewspaperList";
+import React from "react"
+import { useState, useEffect } from "react"
+import Card from "./Card"
 
 const CardList = () => {
-  // // Merge lccnArr and dateArr
-  // let mergedArr = lccnArr.map((item, i) => Object.assign({}, item, dateArr[i]));
+  const [newspapers, setNewspapers] = useState([])
+  const newspapersFetcher = () => {
+    fetch("https://chroniclingamerica.loc.gov/newspapers.json")
+      .then((res) => res.json())
+      .then((jsonData) => {
+        setNewspapers(jsonData.newspapers)
+        console.log("newspapers:")
+        // console.log(jsonData.newspapers);
+      })
+  }
+  useEffect(() => {
+    newspapersFetcher()
+  }, [])
 
-  // console.log(mergedArr);
-  NewspaperList();
   return (
     <div>
       <h2>CardList</h2>
-
-      {NewspaperList.map((item, index) => {
-        return <Card key={index} lccn={item.lccn} data={item} />;
-      })}
+      {/* {NewspaperList.map((item, index) => {
+        return <Card key={index} lccn={item.lccn} data={item} />
+      })} */}
     </div>
-  );
-};
+  )
+}
 
-export default CardList;
+export default CardList
+
+// // Merge lccnArr and dateArr
+// let mergedArr = lccnArr.map((item, i) => Object.assign({}, item, dateArr[i]));
+
+// console.log(mergedArr);
--- src/lib/NewspaperList.jsx
+++ src/lib/NewspaperList.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React from "react"
+import { useState, useEffect } from "react"
 
 const NewspaperList = () => {
-  const [newspaperList, setNewspaperList] = useState([]);
+  const [newspaperList, setNewspaperList] = useState([])
 
   fetch("https://chroniclingamerica.loc.gov/newspapers.json")
-    .then(res => res.json())
-    .then(jsonData => {
-      setNewspaperList(jsonData.newspapers);
-      console.log("newspaper-list:");
+    .then((res) => res.json())
+    .then((jsonData) => {
+      setNewspaperList(jsonData.newspapers)
+      console.log("newspaper-list:")
       // console.log(jsonData.newspapers);
-    });
+    })
 
-  return newspaperList;
-};
+  return null
+}
 
-export default NewspaperList;
+export default NewspaperList
